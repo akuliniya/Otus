808246852608db50c02c28754f2d97bb7a2801e9
akuliniya <67019252+akuliniya@users.noreply.github.com>
2021-10-14 00:24:22 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello, world! It's rainy today( But tomorrow will be sunny)");
+		System.out.println("Hello, world! It's rainy today( But tomorrow will be sunny))))");
 	}
 }
